849a298f9472ddf354fd1c1eda26ddffaf837b06
RahsyaAndika <andhikarasya567@gmail.com>
2024-12-02 14:05:38 +0700

    first up

--- uts/assets/js/script.js
+++ uts/assets/js/script.js
@@ -1,8 +1,13 @@
-function toggleForm() {
-    const form = document.getElementById('login-form');
-    if (form.style.display === 'none' || form.style.display === '') {
-        form.style.display = 'block'; 
-    } else {
-        form.style.display = 'none'; 
-    }
+
+function toggleForm(formType) {
+  const loginForm = document.getElementById('login-form');
+  const registerForm = document.getElementById('register-form');
+
+  if (formType === 'login') {
+      loginForm.style.display = 'block';
+      registerForm.style.display = 'none';
+  } else if (formType === 'register') {
+      loginForm.style.display = 'none';
+      registerForm.style.display = 'block';
+  }
 }
